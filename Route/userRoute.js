49522f356c9d80ead7e1adf414d0b57c6875cade
iyaiyayo26 <fahiyanmr26@gmail.com>
2023-07-28 11:02:15 +0700

    edit userRoute.js

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -2,18 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Model/users');
 
-// post comment
 router.post('/comment/:videoId', async (req, res) => {
     const {username, comment} = req.body;
     const videoId = req.params.videoId;
 
     try {
          if(username === " "){
-          // res.status(400).json({message: 'username tidak boleh kosong'});
           throw new Error('username tidak boleh kosong')
          }
          if(comment === " "){
-          // res.status(400).json({message: 'comment tidak boleh kosong'})
           throw new Error('comment tidak boleh kosong')
          }
         const newUser = new User({username, comment, videoId});
@@ -30,7 +27,6 @@ router.get('/comment/:videoId', async (req, res) => {
     try {
         const videoId = req.params.videoId;
     
-        // Find all users with the specified videoId
         const users = await User.find({ videoId }).exec();
     
         if (users.length === 0) {
@@ -39,7 +35,7 @@ router.get('/comment/:videoId', async (req, res) => {
 
         res.status(200).json(users);
       } catch (err) {
-        res.status(500).json({ error: 'Failed to retrieve user data' });
+        res.status(500).json({ error: 'Gagal mengambil data user' });
       }
 })
 
